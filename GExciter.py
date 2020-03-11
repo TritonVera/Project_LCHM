a5cc91c6ee88f2f815ec3ab4da577ca673156161
TritonVera <vanyafomin@yandex.ru>
2020-03-11 13:48:41 +0300

    SSSS

--- GExciter.py
+++ GExciter.py
@@ -6,8 +6,8 @@ Created on Fri Feb 14 21:21:17 2020
 @author: Григорий
 @author: Ivan
 """
-import sys                              #System function
-import Mimp                             #User interface classes
+import sys                            #System function
+import UI                             #User interface classes
 
 from PyQt5.QtWidgets import QApplication
 from Radiopulse import *
@@ -22,48 +22,42 @@ def close(): #Закрыть
 def plotb(): #Построить
 
     """Вставка функции для реализации пачки радиоимпульсов"""
-    if ui.radioButton_3.isChecked() == True:
-        global m
-        radio = Radiopulse(amplify = ui.doubleSpinBox.value())
-        radio.send_test()
-        del m
-        m = MakePlot(ui.PlotWidget, 3, 3, 100, radio.xpoints, radio.Ipoints)
-        m.move(0, 0)
+    if ui.radio_radiobutton.isChecked() == True:
+        radio = Radiopulse(amplify = ui.ku_spinbox.value())
+        #radio.send_test()
+        ui.plot.draw_plot(radio.xpoints, radio.Ipoints)
         return
     """Конец вставки"""
 
     n = SignalObj(type_of_signal) #Заполнение массивов времени и значения сигнала
-    KU = ui.doubleSpinBox.value()
-    n.UM(KU) #Умножение значений на коэффициент усиления
+    #KU = ui.doubleSpinBox.value()
+    #n.UM(KU) #Умножение значений на коэффициент усиления
     m.plot() #Построение графика
 
 def NLNF(): #НЛЧМ
 
-    if ui.radioButton_2.isChecked() == True:
+    if ui.nlchm_radiobutton.isChecked() == True:
         global type_of_signal
         type_of_signal = "NLNF"
 
 def LNF(): #ЛЧМ
 
-    if ui.radioButton.isChecked() == True:
+    if ui.lchm_radiobutton.isChecked() == True:
         global type_of_signal
         type_of_signal = "LNF"
 
 app = QApplication(sys.argv)
-ui = Mimp.DemoWindow()
+ui = UI.DemoWindow()
 
 """ Реализация конструктора UI """
 #ui = uic.loadUi("Exciter.ui")
 
 #Привязка кнопок
-#ui.pushButton.clicked.connect(close)
-#ui.pushButton_2.clicked.connect(plotb)
-#ui.radioButton.toggled.connect(LNF)
-#ui.radioButton_2.toggled.connect(NLNF)
+ui.ku_spinbox.valueChanged.connect(plotb)
+ui.radio_radiobutton.toggled.connect(plotb)
+#ui.lchm_radioButton.toggled.connect(LNF)
+#ui.nlchm_radioButton.toggled.connect(NLNF)
 
-#Создание виджета графика
-#m = PlotCanvas(ui.PlotWidget, 3, 3, 100)
-#m.move(0, 0)
 """ Конец реализации конструктора """
 
 ui.show()
